42234e3063ee8917a98a761acb81289047338cc2
Matthew Hopson <mhopson@hotmail.co.uk>
2025-07-04 19:46:06 +0100

    Fix the imgui ids in the texture update gui

--- src/Editor/WorldGeometry.cpp
+++ src/Editor/WorldGeometry.cpp
@@ -8,17 +8,20 @@
 
 namespace
 {
-    bool prop_gui(const char* name, TextureProp& texture_prop, const LevelTextures& textures)
+    bool prop_gui(const char* name, TextureProp& texture_prop, const LevelTextures& textures,
+                  int& id)
     {
         bool update = false;
-        ImGui::Text(name);
-        constexpr float image_size = 64.0f;
-        int id = 0;
+        ImGui::Text("%s", name);
         for (const auto& [name, texture] : textures.texture_2d_map)
         {
             ImGui::PushID(id++);
+
             ImGui::SameLine();
-            if (ImGui::ImageButton(name.c_str(), static_cast<ImTextureID>(texture.id), {32, 32}))
+            std::string button_id = name + "###" + std::to_string(id);
+
+            if (ImGui::ImageButton(button_id.c_str(), static_cast<ImTextureID>(texture.id),
+                                   {32, 32}))
             {
                 if (auto texture_id = textures.get_texture(name))
                 {
@@ -28,6 +31,7 @@ namespace
 
                 std::println("Texture clicked: {}", name);
             }
+
             if (ImGui::IsItemHovered())
             {
                 ImGui::SetTooltip("%s", name.c_str());
@@ -43,20 +47,23 @@ bool Wall::property_gui(EditorState& state, const LevelTextures& textures)
     bool update = false;
     if (ImGui::Begin("Wall Properties"))
     {
+        int button_id = 0;
         ImGui::Separator();
-        update |= prop_gui("Side 1", props.texture_side_1, textures);
+        update |= prop_gui("Side 1", props.texture_side_1, textures, button_id);
 
         ImGui::Separator();
-        // update |= prop_gui("Side 2", props.texture_side_2, textures);
+        update |= prop_gui("Side 2", props.texture_side_2, textures, button_id);
 
         ImGui::Separator();
         if (ImGui::Button("Set As Default"))
         {
             state.wall_default.texture_side_1 = props.texture_side_1;
-           // state.wall_default.texture_side_2 = props.texture_side_2;
+            state.wall_default.texture_side_2 = props.texture_side_2;
         }
     }
     ImGui::End();
-    std::print("Update: {}", update);
+
+    ImGui::ShowDemoWindow();
+
     return update;
 }
