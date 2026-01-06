54fef840c16a6d27430a16a2401b3a917f2b7828
Matthew Hopson <mhopson@hotmail.co.uk>
2026-01-06 21:41:23 +0000

    Change free functions for 3d camera input to be member functions

--- src/Graphics/CameraController.cpp
+++ src/Graphics/CameraController.cpp
@@ -8,84 +8,17 @@
 #include "../Util/Keyboard.h"
 #include "Camera.h"
 
-namespace
-{
-    glm::vec3 keyboard_input(const Keyboard& keyboard, const Camera& camera,
-                             const CameraKeybinds& keybinds, CameraControllerOptions3D options)
-    {
-        glm::vec3 move{0.0f};
-        auto flat = !options.free_movement || camera.get_type() != CameraType::Perspective;
-        if (keyboard.is_key_down(keybinds.forward))
-        {
-            move += flat ? forward_vector_flat(camera.transform.rotation)
-                         : forward_vector(camera.transform.rotation);
-        }
-        else if (keyboard.is_key_down(keybinds.back))
-        {
-            move += flat ? backward_vector_flat(camera.transform.rotation)
-                         : backward_vector(camera.transform.rotation);
-        }
-
-        if (keyboard.is_key_down(keybinds.left))
-        {
-            move += left_vector(camera.transform.rotation);
-        }
-        else if (keyboard.is_key_down(keybinds.right))
-        {
-            move += right_vector(camera.transform.rotation);
-        }
-
-        if (keyboard.is_key_down(keybinds.up))
-        {
-            move.y += 1.5f;
-        }
-        else if (keyboard.is_key_down(keybinds.down))
-        {
-            move.y -= 1.5f;
-        }
-
-        move *= 4.0f;
-
-        // Speed up when left shift is held
-        if (keyboard.is_key_down(sf::Keyboard::Key::LShift))
-        {
-            move *= 4.0f;
-        }
-
-        return move;
-    }
-
-    void mouse_input(const sf::Window& window, Camera& camera, sf::Vector2i& last_mouse_position, float look_sensitivity)
-    {
-        auto change = sf::Mouse::getPosition(window) - last_mouse_position;
-        auto& r = camera.transform.rotation;
-
-        r.x -= static_cast<float>(change.y) * look_sensitivity;
-        r.y += static_cast<float>(change.x) * look_sensitivity;
-
-        sf::Mouse::setPosition({(int)window.getSize().x / 2, (int)window.getSize().y / 2}, window);
-        last_mouse_position = sf::Mouse::getPosition(window);
-
-        r.x = glm::clamp(r.x, -89.9f, 89.9f);
-        if (r.y >= 360.0f)
-        {
-            r.y = 0.0f;
-        }
-        else if (r.y < 0.0f)
-        {
-            r.y = 359.9f;
-        }
-    }
-} // namespace
-
-CameraController3D::CameraController3D(Camera& camera, const CameraKeybinds& keybinds)
+CameraController3D::CameraController3D(Camera& camera, const CameraKeybinds& keybinds,
+                                       const CameraControllerOptions3D& options)
     : p_camera_{&camera}
     , p_keybinds_{&keybinds}
+    , p_camera_options_(&options)
 {
 }
 
 void CameraController3D::handle_events(const sf::Event& event)
 {
+    // Ensures that the camera look does not jerk when the middle mouse button is pressed
     if (auto mouse = event.getIf<sf::Event::MouseMoved>())
     {
         if (!middle_mouse_down_)
@@ -110,27 +43,95 @@ void CameraController3D::handle_events(const sf::Event& event)
     }
 }
 
-void CameraController3D::handle_inputs(const Keyboard& keyboard, sf::Time dt, sf::Window& window,
-                                       const CameraControllerOptions3D& options)
+
+void CameraController3D::handle_inputs(const Keyboard& keyboard, sf::Time dt, sf::Window& window)
 {
     if (!window.hasFocus())
     {
         return;
     }
 
-    auto move = keyboard_input(keyboard, *p_camera_, *p_keybinds_, options);
+    auto move = get_keybord_input(keyboard);
     move.y = p_camera_->get_type() == CameraType::Perspective ? move.y : 0;
     p_camera_->transform.position += move * dt.asSeconds();
     p_camera_->update();
 
-    if (!options.lock_rotation || middle_mouse_down_)
+    if (!p_camera_options_->lock_rotation || middle_mouse_down_)
     {
-        mouse_input(window, *p_camera_, last_mouse_position_, options.look_sensitivity);
+        handle_look(window);
     }
 
-    if (options.lock_rotation)
+    if (p_camera_options_->lock_rotation)
     {
         window.setMouseCursorVisible(!middle_mouse_down_);
+    }
+}
+
+glm::vec3 CameraController3D::get_keybord_input(const Keyboard& keyboard)
+{
+    glm::vec3 move{0.0f};
+
+    auto flat_movement = !p_camera_options_->free_movement || p_camera_->get_type() != CameraType::Perspective;
+
+    if (keyboard.is_key_down(p_keybinds_->forward))
+    {
+        move += flat_movement ? forward_vector_flat(p_camera_->transform.rotation)
+                     : forward_vector(p_camera_->transform.rotation);
+    }
+    else if (keyboard.is_key_down(p_keybinds_->back))
+    {
+        move += flat_movement ? backward_vector_flat(p_camera_->transform.rotation)
+                     : backward_vector(p_camera_->transform.rotation);
+    }
+
+    if (keyboard.is_key_down(p_keybinds_->left))
+    {
+        move += left_vector(p_camera_->transform.rotation);
+    }
+    else if (keyboard.is_key_down(p_keybinds_->right))
+    {
+        move += right_vector(p_camera_->transform.rotation);
+    }
+
+    if (keyboard.is_key_down(p_keybinds_->up))
+    {
+        move.y += 1.5f;
+    }
+    else if (keyboard.is_key_down(p_keybinds_->down))
+    {
+        move.y -= 1.5f;
+    }
+
+    move *= 4.0f;
+
+    // Speed up when left shift is held
+    if (keyboard.is_key_down(sf::Keyboard::Key::LShift))
+    {
+        move *= 4.0f;
+    }
+
+    return move;
+}
+
+void CameraController3D::handle_look(const sf::Window& window)
+{
+    auto change = sf::Mouse::getPosition(window) - last_mouse_position_;
+    auto& r = p_camera_->transform.rotation;
+
+    r.x -= static_cast<float>(change.y) * p_camera_options_->look_sensitivity;
+    r.y += static_cast<float>(change.x) * p_camera_options_->look_sensitivity;
+
+    sf::Mouse::setPosition({(int)window.getSize().x / 2, (int)window.getSize().y / 2}, window);
+    last_mouse_position_ = sf::Mouse::getPosition(window);
+
+    r.x = glm::clamp(r.x, -89.9f, 89.9f);
+    if (r.y >= 360.0f)
+    {
+        r.y = 0.0f;
+    }
+    else if (r.y < 0.0f)
+    {
+        r.y = 359.9f;
     }
 }
 
